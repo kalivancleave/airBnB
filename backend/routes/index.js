acd53dbc7956845ca33b67f831db43ea21082782
kalivancleave <kalivancleave@gmail.com>
2024-06-04 07:56:01 -0700

    initialize express and sequelize with csrf protection

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-
-
-
-
-
 //test route
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
+//re-set CSRF token cookie route
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
 
 
 
