309280d4c6d45e46111ed7b88d4b79c8491e420d
kalivancleave <kalivancleave@gmail.com>
2024-06-04 20:27:36 -0700

    add user login backend endpoint

---
+++ backend/routes/api/session.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+  try {
+    //destructure credential and password from the req.body
+    const {credential, password} = req.body;
+
+    //define the unscoped user that is found with that credential
+    const user = await User.unscoped().findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential
+        }
+      }
+    });
+
+    //if there is no user or the password does not match throw an error
+    if(!user || !bcrypt.compareSync(password, user.hashedPassword.toString())){
+      const error = new Error('Login failed');
+      error.status = 401;
+      error.title = 'Login failed';
+      error.errors = { credential: 'The provided credentials were invalid.'}
+      return next(error);
+    };
+
+    //password and credential matches a user
+    //create safe user info for display
+    const safeUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username
+    }
+
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
+      user: safeUser
+    });
+    
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+module.exports = router;
