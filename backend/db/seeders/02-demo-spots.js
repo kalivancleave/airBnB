9196214c0d3197806e56e07bbd3e43ecefbc6017
kalivancleave <kalivancleave@gmail.com>
2024-06-06 19:41:43 -0700

    get spot by current user id

--- backend/db/seeders/02-demo-spots.js
+++ backend/db/seeders/02-demo-spots.js
@@ -41,6 +41,18 @@ const spots = [
     description: 'A third option for a wonderful place to stay.',
     price: 2.00
   },
+  {
+    ownerId: 4,
+    address: '123 Disney Lane',
+    city: 'San Francisco',
+    state: 'California',
+    country: 'United States of America',
+    lat: 37.7645358,
+    lng: -122.4730327,
+    name: 'App Academy',
+    description: 'Place where web developers are created.',
+    price: 123.00
+  },
 ]
 
 /** @type {import('sequelize-cli').Migration} */
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,8 +3,8 @@ const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { Spot, SpotImage, Review } = require('../../db/models');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { Spot, SpotImage, Review, User } = require('../../db/models');
 
 const router = express.Router();
 
@@ -74,12 +74,101 @@ router.get('/', async(req, res, next) => {
       }
 
       updatedSpots.push(payload);
-
     }
     //return requested result
     res.json({
       Spots: updatedSpots
     });
+    
+  } catch (error) {
+    next(error)
+  }
+});
+
+//get all spots owned by the current user
+router.get('/current', requireAuth, async (req, res, next) => {
+  try {
+    //figure out current user id
+    const userId = req.user.id
+    const user = await User.findByPk(userId);
+
+    //404 - no user
+    if(!user){
+      res.status(404),
+      res.json({
+        message: 'No user found'
+      })
+    };
+
+    //find all spots where owner id is user id
+    const spots = await Spot.findAll({
+      where: {
+        ownerId: userId
+      }
+    });
+
+    let updatedSpots = []
+    //iterate through the spots
+    for (let i = 0; i < spots.length; i++) {
+      let spot = spots[i]
+
+      //find all reviews with that spotId
+      const reviews = await Review.findAll({
+        where: {
+          spotId: spot.id
+        }
+      });
+
+      //find the sum of all stars
+      let sumOfStars = 0;
+      for (let i = 0; i < reviews.length; i++){
+        let review = reviews[i]
+        let starRating = review.stars
+        sumOfStars += starRating
+      }
+      
+      //find the average of all the stars from the review table
+      const averageRating = sumOfStars/reviews.length
+
+      //find the preview image with that spotId
+      const previewImage = await SpotImage.findOne({
+        attributes: ['url'],
+        where: {
+          spotId: spot.id
+        }
+      });
+
+      const payload = {
+      id: spot.id,
+      ownerId: spot.ownerId,
+        address: spot.address,
+        city: spot.city,
+        state: spot.state,
+        country: spot.country,
+        lat: spot.lat,
+        lng: spot.lng,
+        name: spot.name,
+        description: spot.description,
+        price: spot.price,
+        createdAt: spot.createdAt,
+        updatedAt: spot.updatedAt,
+        avgRating: averageRating || 'No reviews found',
+        previewImage: previewImage
+      }
+
+      //404 - no image
+      if(!previewImage){
+        payload.previewImage = "No preview image found"
+      } else {
+        payload.previewImage = previewImage.url
+      }
+
+      updatedSpots.push(payload);
+    }
+
+    res.json({
+      Spots: updatedSpots
+    })
     
   } catch (error) {
     next(error)
