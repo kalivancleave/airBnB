e15dd510998cb08a853d0a5b7bbe855cb7711ac6
kalivancleave <kalivancleave@gmail.com>
2024-06-14 07:16:06 -0700

    updated delete 403 response message

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -267,7 +267,7 @@ router.delete('/:bookingId', requireAuth, async(req, res, next) => {
     if(booking.userId !== req.user.id && spot.ownerId !== req.user.id){
       res.status(403)
       return res.json({
-        message: "You are not authorized to delete this booking."
+        message: "Forbidden"
       })
     };
 
