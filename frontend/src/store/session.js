4d0f3fa6439e1820a626389e8d734bf9f24dc32f
kalivancleave <kalivancleave@gmail.com>
2024-07-19 16:59:27 -0700

    signup actions completed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -54,6 +54,24 @@ export const restoreUser = () => async (dispatch) => {
   return response;
 };
 
+//signup a user
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch('/api/users', {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password
+    })
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response
+}
+
 /////////////////////////////////////////
 //reducer
 const sessionReducer = (state = {user: null}, action) => {
