425357105c4f67d656222172527726ef7d2f4ad3
kalivancleave <kalivancleave@gmail.com>
2024-07-20 06:03:28 -0700

    basic nav bar started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
-import * as sessionActions from './store/session';
 import SignupFormPage from './components/SignupFormPage';
-
+import Navigation from './components/Navigation';
+import * as sessionActions from './store/session';
 
 function Layout() {
   const dispatch = useDispatch();
@@ -18,6 +18,7 @@ function Layout() {
 
   return (
     <>
+      <Navigation isLoaded={isLoaded} />
       {isLoaded && <Outlet />}
     </>
   );
---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,11 @@
+import { IoPersonCircleSharp } from "react-icons/io5";
+
+function ProfileButton() {
+  return(
+    <div style={{color: "orange", fontSize: "100px"}}>
+      <IoPersonCircleSharp />
+    </div>
+  )
+}
+
+export default ProfileButton
