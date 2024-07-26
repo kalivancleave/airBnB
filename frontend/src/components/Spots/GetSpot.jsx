3e426e4dd2421240ee97d1fd355339a9228081a2
kalivancleave <kalivancleave@gmail.com>
2024-07-25 21:26:20 -0700

    some more work on spot details

--- frontend/src/components/Spots/GetSpot.jsx
+++ frontend/src/components/Spots/GetSpot.jsx
@@ -5,6 +5,8 @@ import { fetchSpots } from "../../store/spots";
 import { fetchSpot } from "../../store/spots";
 import { fetchSpotOwner } from '../../store/spots'
 import { cloudinaryPreviewImage } from "../../App";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 
 const SingleSpot = () => {
@@ -22,29 +24,38 @@ const SingleSpot = () => {
   
   return (
     <>
-      <div>
-        <h1>Single Spot</h1>
-        <h2>{singleSpot?.name}</h2>
-        <h2>{singleSpot?.city}, {singleSpot?.state}, {singleSpot?.country}</h2>
+      <div className="topMargin leftAndRightPadding">
+        <h1 className="sans noMargin">{singleSpot?.name}</h1>
+        <p className="sans noMargin mediumFont topPadding">{singleSpot?.city}, {singleSpot?.state}, {singleSpot?.country}</p>
       </div>
-      <ol>
+      <ol className="displayFlex">
         {spotImageDetails?.map(({id, url}) => (
-          <div key={id}>
-            <li className="smallSize">{cloudinaryPreviewImage(url)}</li>
+          <div className="leftAndRightPadding" key={id}>
+            <li>{cloudinaryPreviewImage(url)}</li>
           </div>
         ))}
       </ol>
-      <div>
-        <h2>Hosted by {spotOwnerDetails?.firstName} {spotOwnerDetails?.lastName}</h2>
-        {console.log(spotOwnerDetails)}
-        <textarea>Lorem ipsum about the space</textarea>
-      </div>
-      <div>
-        <h2>${singleSpot?.price} night</h2>
-        <h2>{singleSpot?.avgRating}</h2>
-        <h2>number of reviews</h2>
-        <button>Reserve</button>
-      </div>
+        <div>
+          <p className='sans largeFont leftAndRightPadding'>Hosted by {spotOwnerDetails?.firstName} {spotOwnerDetails?.lastName}</p>
+        </div>
+
+        <div className="displayFlex spaceBetween leftAndRightPadding">
+          <textarea className='xlargeSize leftAndRightPadding noBorder'>Lorem ipsum about the space</textarea>
+          
+          <div className="displayFlex fullPadding flexColumn whiteBackground roundedCorners blackBorder mediumSize">
+            <div className="fullMargin displayFlex flexRow spaceBetween">
+              <li className="blackText largeFont leftAndRightPadding sans"> ${singleSpot?.price} night</li>
+              
+              <div className="displayFlex alignCenter">
+                <FontAwesomeIcon icon={faStar} className="redText largeFont" /> 
+                <li className="displayInline fullPadding blackText largeFont sans">{typeof avgRating === 'number' ? avgRating : 'New'}</li>
+              </div>
+            </div>
+                
+            <button className="activeButtonDesign ">Reserve</button>
+          </div>
+        </div>
+  
       <div>
         <h2>page separator -----------------------------------------</h2>
       </div>
