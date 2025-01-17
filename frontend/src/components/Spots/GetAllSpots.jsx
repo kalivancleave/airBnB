1754f76885d886fa703219f2d77366a5fe0608e3
kalivancleave <kalivancleave@gmail.com>
2025-01-17 07:58:00 -0800

    updated styles

--- frontend/src/components/Spots/GetAllSpots.jsx
+++ frontend/src/components/Spots/GetAllSpots.jsx
@@ -18,7 +18,7 @@ const GetAllSpots = () => {
   
   return (
     <>
-      <ol className="displayFlex flexWrap noMargin noPadding fullSize justifyLeft">
+      <ol className="displayFlex flexWrap noMargin noPadding fullSize justifyLeft blackBorder">
       <Tooltip id='my-tooltip' />
 
         {spotsList.map(({id, name, previewImage, city, state, avgRating, price}) => (
